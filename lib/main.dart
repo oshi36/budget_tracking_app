2217ceafc2dd8277b7e96525dfc535b946413a9b
Oshi Gupta <oshiagupta36@gmail.com>
2021-04-18 17:26:00 +0530

    budget app files added

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_dev/pages/root_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
